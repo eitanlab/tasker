2cfc5273ca60e98ee0b850ca1d5e2642a150a1ec
Eitan Lab <46536280+eitanlab@users.noreply.github.com>
2021-06-29 00:11:37 -0300

    Agrego MaterialUI al proyecto, aplico componentes y estilos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import {Container, Box, Paper, Button, TextField} from '@material-ui/core';
+import { spacing } from '@material-ui/system';
 
 const App = () => {
   const [todoItems, setTodoItems] = useState([]);
@@ -14,16 +16,26 @@ const App = () => {
 	}
 
   return (
-    <div className="App">
-      <input onChange={e => setInputValue(e.target.value)} value={inputValue}></input>
-      <button onClick={e => addTodoItem({content: inputValue})}>Add Item</button>
+    <Container maxWidth='lg' className="App">
+      <Box display='flex' py={2}>
+				<TextField style={{minWidth: '300px', padding: '0 20px 0 0'}} label='Por ejemplo: Ir a la playa' onChange={e => setInputValue(e.target.value)} value={inputValue}></TextField>
+				<Button variant='contained' color='primary' onClick={e => addTodoItem({content: inputValue})}>
+					Add Item
+				</Button>
+			</Box>
 			{todoItems.map((item) => 
-			<div key={item.id}>
-				<h2>{item.id}</h2>
-				<p>{item.content}</p>
-				<button onClick={() => deleteTodoItem({id: item.id})}>Delete item</button>
-			</div>)}
-    </div>
+				<Paper style={{margin: '20px 0'}} key={item.id}>
+					<Box display='flex' style={{paddingLeft: '20px'}}>
+						<Box flexGrow={1}>
+							<h2 style={{minWidth: '300px', padding: '0 20px'}}>{item.id} - {item.content}</h2>
+						</Box>
+						<Button variant='contained' color='secondary' onClick={() => deleteTodoItem({id: item.id})}>
+							Delete item
+						</Button>
+					</Box>	
+				</Paper>
+			)}
+    </Container>
   );
 }
 
